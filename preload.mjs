f2d615e7c061f5b6c93a018802bb5bca97c29d4b
Mico-rstar <2114678406@qq.com>
2025-02-11 11:59:43 +0800

    修复了文件保存和恢复的一些bug

--- preload.mjs
+++ preload.mjs
@@ -6,13 +6,18 @@ import { contextBridge, ipcRenderer } from "electron"
 
 
 
+
+
 console.log('Exposing blocksuite to window');
 contextBridge.exposeInMainWorld('DRAPI', {
     // create: () => createEmptyDoc,
     // editor: () => PageEditor,
     // text: () => Text
     node: () => process.versions.node,
-    saveFile: (callback) => ipcRenderer.on('save-file', (event, value) => callback(value))
+    saveFile: (callback) => ipcRenderer.on('save-file', (event, value) => callback(value)),
+    recoverFile: (callback) => ipcRenderer.on('recover-file', (event, value) => callback(value)),
+    write: (filePath, data) => ipcRenderer.invoke('writeFile', filePath, data),
+    read: (filePath) => ipcRenderer.invoke('readFile', filePath)
 
 });
 
